e48497b94b7d3685fadbb29e95614e7eba531eeb
koki-develop <kou.pg.0131@gmail.com>
2022-03-01 19:55:25 +0900

    extends に prettier を追加

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
     "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
