a0dd7f83ff44a273bb71b11321ed7dd5b63c9379
koki-develop <kou.pg.0131@gmail.com>
2022-03-01 20:45:20 +0900

    プロジェクト読み込み処理を仮実装

--- frontend/src/components/pages/ProjectPage/ProjectPage.tsx
+++ frontend/src/components/pages/ProjectPage/ProjectPage.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Project } from "@/models/project";
 
 const ProjectPage: React.VFC = React.memo(() => {
-  return <div>project page</div>;
+  const params = useParams();
+  const id = params.id as string;
+
+  const [projectLoaded, setProjectLoaded] = useState<boolean>(false);
+  const [project, setProject] = useState<Project | null>(null);
+
+  useEffect(() => {
+    setProjectLoaded(false);
+    const timeoutId = setTimeout(() => {
+      setProjectLoaded(true);
+      setProject({ id, name: "sample project" });
+    }, 500);
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [id]);
+
+  if (!projectLoaded) {
+    return <div>loading...</div>;
+  }
+
+  return (
+    <div>
+      <div>id: {id}</div>
+    </div>
+  );
 });
 
 ProjectPage.displayName = "ProjectPage";
