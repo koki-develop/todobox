e347e342546b249f68b321518044953b9ef7bcdf
koki-develop <kou.pg.0131@gmail.com>
2022-03-01 20:41:01 +0900

    ProjectPage を仮作成

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "@/components/pages/LoginPage";
 import NotFoundPage from "@/components/pages/NotFoundPage";
 import ProjectsPage from "@/components/pages/ProjectsPage";
+import ProjectPage from "@/components/pages/ProjectPage";
 
 const App: React.VFC = React.memo(() => {
   return (
@@ -10,6 +11,7 @@ const App: React.VFC = React.memo(() => {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/projects/:id" element={<ProjectPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
---
+++ frontend/src/components/pages/ProjectPage/ProjectPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ProjectPage: React.VFC = React.memo(() => {
+  return <div>project page</div>;
+});
+
+ProjectPage.displayName = "ProjectPage";
+
+export default ProjectPage;
