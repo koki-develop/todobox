0539f5c1ad6978beaca7645cc67d37c13e90bcea
koki-develop <kou.pg.0131@gmail.com>
2022-03-01 20:04:15 +0900

    ProjectsPage コンポーネントを仮作成

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import ProjectsPage from "@/components/pages/ProjectsPage";
 
 const App: React.VFC = React.memo(() => {
-  return <div>hello world</div>;
+  return (
+    <div>
+      <ProjectsPage />
+    </div>
+  );
 });
 
 App.displayName = "App";
---
+++ frontend/src/components/pages/ProjectsPage/ProjectsPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ProjectsPage: React.VFC = React.memo(() => {
+  return <div>projects page</div>;
+});
+
+ProjectsPage.displayName = "ProjectsPage";
+
+export default ProjectsPage;
