74ea238f5f0744bce12be48ff065a9f45582dc45
koki-develop <kou.pg.0131@gmail.com>
2022-03-01 20:15:47 +0900

    NotFoundPage を仮作成

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "@/components/pages/LoginPage";
+import NotFoundPage from "@/components/pages/NotFoundPage";
 import ProjectsPage from "@/components/pages/ProjectsPage";
 
 const App: React.VFC = React.memo(() => {
@@ -9,6 +10,7 @@ const App: React.VFC = React.memo(() => {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/components/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NotFoundPage: React.VFC = React.memo(() => {
+  return <div>not found</div>;
+});
+
+NotFoundPage.displayName = "NotFoundPage";
+
+export default NotFoundPage;
