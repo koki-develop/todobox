08d0dc69d601d897fdad5500291c4e0ef278323f
koki-develop <kou.pg.0131@gmail.com>
2022-03-01 20:59:01 +0900

    セクション・タスクを表示できるところまで

--- frontend/src/components/pages/ProjectPage/ProjectPage.tsx
+++ frontend/src/components/pages/ProjectPage/ProjectPage.tsx
@@ -1,6 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
+import { ulid } from "ulid";
 import { Project } from "@/models/project";
+import { Section } from "@/models/section";
+import { Task } from "@/models/task";
+
+const dummySections: Section[] = [
+  { id: ulid(), name: "section 1" },
+  { id: ulid(), name: "section 2" },
+  { id: ulid(), name: "section 3" },
+];
+
+const dummyTasks: Task[] = [
+  { sectionId: null, id: ulid(), title: "task 1" },
+  { sectionId: null, id: ulid(), title: "task 2" },
+  { sectionId: dummySections[0].id, id: ulid(), title: "task 3" },
+  { sectionId: dummySections[0].id, id: ulid(), title: "task 4" },
+  { sectionId: dummySections[1].id, id: ulid(), title: "task 5" },
+  { sectionId: dummySections[1].id, id: ulid(), title: "task 6" },
+  { sectionId: dummySections[2].id, id: ulid(), title: "task 7" },
+  { sectionId: dummySections[2].id, id: ulid(), title: "task 8" },
+  { sectionId: dummySections[2].id, id: ulid(), title: "task 9" },
+  { sectionId: dummySections[2].id, id: ulid(), title: "task 10" },
+];
 
 const ProjectPage: React.VFC = React.memo(() => {
   const params = useParams();
@@ -8,12 +30,16 @@ const ProjectPage: React.VFC = React.memo(() => {
 
   const [projectLoaded, setProjectLoaded] = useState<boolean>(false);
   const [project, setProject] = useState<Project | null>(null);
+  const [sections, setSections] = useState<Section[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   useEffect(() => {
     setProjectLoaded(false);
     const timeoutId = setTimeout(() => {
       setProjectLoaded(true);
       setProject({ id, name: "sample project" });
+      setSections(dummySections);
+      setTasks(dummyTasks);
     }, 500);
     return () => {
       clearTimeout(timeoutId);
@@ -24,7 +50,40 @@ const ProjectPage: React.VFC = React.memo(() => {
     return <div>loading...</div>;
   }
 
-  return <div>{project && <div>{project.name}</div>}</div>;
+  return (
+    <div>
+      {project && (
+        <div>
+          <div>{project.name}</div>
+          <div>
+            <div>
+              <ul>
+                {tasks
+                  .filter((task) => task.sectionId == null)
+                  .map((task) => (
+                    <li key={task.id}>{task.title}</li>
+                  ))}
+              </ul>
+            </div>
+            <div>
+              {sections.map((section) => (
+                <div key={section.id}>
+                  <div>{section.name}</div>
+                  <ul>
+                    {tasks
+                      .filter((task) => task.sectionId === section.id)
+                      .map((task) => (
+                        <li key={task.id}>{task.title}</li>
+                      ))}
+                  </ul>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 });
 
 ProjectPage.displayName = "ProjectPage";
