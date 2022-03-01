da4edd85c2a6795bff17bf86237a8bccfe8ed208
koki-develop <kou.pg.0131@gmail.com>
2022-03-01 20:06:04 +0900

    LoginPage を仮作成

---
+++ frontend/src/components/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LoginPage: React.VFC = React.memo(() => {
+  return <div>login page</div>;
+});
+
+LoginPage.displayName = "LoginPage";
+
+export default LoginPage;
