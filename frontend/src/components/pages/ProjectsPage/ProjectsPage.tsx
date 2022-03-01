9d04d7174f057d13034a5d5edf1a7e57e58e1c8a
koki-develop <kou.pg.0131@gmail.com>
2022-03-01 20:36:22 +0900

    プロジェクト作成の処理を仮実装

--- frontend/src/components/pages/ProjectsPage/ProjectsPage.tsx
+++ frontend/src/components/pages/ProjectsPage/ProjectsPage.tsx
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { ulid } from "ulid";
+import { Project } from "@/models/project";
+
+const dummyProjects: Project[] = [
+  { id: ulid(), name: "project 1" },
+  { id: ulid(), name: "project 2" },
+];
 
 const ProjectsPage: React.VFC = React.memo(() => {
-  return <div>projects page</div>;
+  const [projectsLoaded, setProjectsLoaded] = useState<boolean>(false);
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  const [name, setName] = useState<string>("");
+
+  const handleChangeName = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setName(e.currentTarget.value);
+    },
+    []
+  );
+
+  const handleCreateProject = useCallback(() => {
+    const trimmedName = name.trim();
+    if (trimmedName === "") return;
+    setName("");
+    const project: Project = { id: ulid(), name: trimmedName };
+    setProjects((prev) => [...prev, project]);
+  }, [name]);
+
+  useEffect(() => {
+    setProjectsLoaded(false);
+    const timeoutId = setTimeout(() => {
+      setProjectsLoaded(true);
+      setProjects(dummyProjects);
+    }, 500);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+  if (!projectsLoaded) {
+    return <div>loading...</div>;
+  }
+
+  return (
+    <div>
+      <div>
+        <input type="text" value={name} onChange={handleChangeName} />
+        <button onClick={handleCreateProject}>create</button>
+      </div>
+
+      <div>
+        <ul>
+          {projects.map((project) => (
+            <li key={project.id}>{project.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 });
 
 ProjectsPage.displayName = "ProjectsPage";
