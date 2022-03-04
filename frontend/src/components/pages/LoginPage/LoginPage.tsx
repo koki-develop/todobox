db4be61a6f9820926f49de554dc5e6b37ae0b6d2
koki-develop <kou.pg.0131@gmail.com>
2022-03-04 20:25:54 +0900

    ログイン画面を雑実装

--- frontend/src/components/pages/LoginPage/LoginPage.tsx
+++ frontend/src/components/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,60 @@
-import React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/material/styles";
+import React, { useCallback } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Field = styled(Box)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "center",
+  ":not(:last-child)": {
+    marginBottom: theme.spacing(2),
+  },
+}));
 
 const LoginPage: React.VFC = React.memo(() => {
-  return <div>login page</div>;
+  const navigate = useNavigate();
+
+  const handleClickLoginWithGoogle = useCallback(() => {
+    navigate("/projects", { replace: true });
+  }, [navigate]);
+
+  const handleClickLoginAnonymously = useCallback(() => {
+    navigate("/projects", { replace: true });
+  }, [navigate]);
+
+  return (
+    <Container maxWidth="sm" sx={{ mt: 20 }}>
+      <Paper sx={{ p: 4 }} elevation={10}>
+        <Field>
+          <Typography>TODO BOX</Typography>
+        </Field>
+
+        <Field>
+          <Button
+            fullWidth
+            variant="contained"
+            onClick={handleClickLoginWithGoogle}
+          >
+            Google でログイン
+          </Button>
+        </Field>
+
+        <Field>
+          <Button
+            fullWidth
+            variant="contained"
+            onClick={handleClickLoginAnonymously}
+          >
+            ゲストログイン
+          </Button>
+        </Field>
+      </Paper>
+    </Container>
+  );
 });
 
 LoginPage.displayName = "LoginPage";
