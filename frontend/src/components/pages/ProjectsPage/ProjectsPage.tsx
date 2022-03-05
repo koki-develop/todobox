0813ee4ac4a7111e283f8f9e72551419e9b96a22
koki-develop <kou.pg.0131@gmail.com>
2022-03-05 15:22:27 +0900

    とりあえずプロジェクトを取得・作成できるところまで

--- frontend/src/components/pages/ProjectsPage/ProjectsPage.tsx
+++ frontend/src/components/pages/ProjectsPage/ProjectsPage.tsx
@@ -1,20 +1,18 @@
 import { signOut, User } from "firebase/auth";
+import { collection, doc, onSnapshot, setDoc } from "firebase/firestore";
 import React, { useCallback, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { ulid } from "ulid";
 import { Project } from "@/models/project";
-import { auth } from "@/lib/firebase";
-
-const dummyProjects: Project[] = [
-  { id: ulid(), name: "project 1" },
-  { id: ulid(), name: "project 2" },
-];
+import { auth, firestore } from "@/lib/firebase";
 
 export type ProjectsPageProps = {
   currentUser: User;
 };
 
 const ProjectsPage: React.VFC<ProjectsPageProps> = React.memo((props) => {
+  const { currentUser } = props;
+
   const [projectsLoaded, setProjectsLoaded] = useState<boolean>(false);
   const [projects, setProjects] = useState<Project[]>([]);
 
@@ -31,21 +29,23 @@ const ProjectsPage: React.VFC<ProjectsPageProps> = React.memo((props) => {
     const trimmedName = name.trim();
     if (trimmedName === "") return;
     setName("");
-    const project: Project = { id: ulid(), name: trimmedName };
-    setProjects((prev) => [...prev, project]);
-  }, [name]);
+    const id = ulid();
+    const project: Omit<Project, "id"> = { name: trimmedName };
+
+    const ref = doc(firestore, "users", currentUser.uid, "projects", id);
+    setDoc(ref, project);
+  }, [currentUser.uid, name]);
 
   useEffect(() => {
-    setProjectsLoaded(false);
-    const timeoutId = setTimeout(() => {
+    const ref = collection(firestore, "users", currentUser.uid, "projects");
+    const unsubscribe = onSnapshot(ref, (snapshot) => {
       setProjectsLoaded(true);
-      setProjects(dummyProjects);
-    }, 500);
-
-    return () => {
-      clearTimeout(timeoutId);
-    };
-  }, []);
+      setProjects(
+        snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() } as Project))
+      );
+    });
+    return unsubscribe;
+  }, [currentUser.uid]);
 
   if (!projectsLoaded) {
     return <div>loading...</div>;
