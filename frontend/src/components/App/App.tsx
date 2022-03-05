2eaea660627ccc1bcfc345e1e8b243055af7d23f
koki-develop <kou.pg.0131@gmail.com>
2022-03-05 14:42:47 +0900

    AuthProvider コンポーネントを削除

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -2,18 +2,15 @@ import CssBaseline from "@mui/material/CssBaseline";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
-import AuthProvider from "@/components/providers/AuthProvider";
 import AppRoutes from "./AppRoutes";
 
 const App: React.VFC = React.memo(() => {
   return (
     <RecoilRoot>
-      <AuthProvider>
-        <CssBaseline />
-        <BrowserRouter>
-          <AppRoutes />
-        </BrowserRouter>
-      </AuthProvider>
+      <CssBaseline />
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </RecoilRoot>
   );
 });
