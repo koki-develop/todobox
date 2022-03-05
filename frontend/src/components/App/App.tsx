bcbcb37f93a4b852d7238d8457642e3cfa39a0f1
koki-develop <kou.pg.0131@gmail.com>
2022-03-05 11:48:52 +0900

    AuthProvider を設置

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -2,15 +2,18 @@ import CssBaseline from "@mui/material/CssBaseline";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import AuthProvider from "@/components/providers/AuthProvider";
 import AppRoutes from "./AppRoutes";
 
 const App: React.VFC = React.memo(() => {
   return (
     <RecoilRoot>
-      <CssBaseline />
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
+      <AuthProvider>
+        <CssBaseline />
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </AuthProvider>
     </RecoilRoot>
   );
 });
