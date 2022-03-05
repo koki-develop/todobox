0a5e88f0e6e18ec0383f54e3b8b8f526223e9070
koki-develop <kou.pg.0131@gmail.com>
2022-03-05 22:29:34 +0900

    dummySections を削除

--- frontend/src/models/section.ts
+++ frontend/src/models/section.ts
@@ -1,11 +1,3 @@
-import { ulid } from "ulid";
-
-export const dummySections: Section[] = [
-  { projectId: "dummyprojectid", id: ulid(), index: 0, name: "section 1" },
-  { projectId: "dummyprojectid", id: ulid(), index: 1, name: "section 2" },
-  { projectId: "dummyprojectid", id: ulid(), index: 2, name: "section 3" },
-];
-
 export type Section = {
   projectId: string;
 
