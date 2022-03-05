9a4c558f5ac093671f6dafd7074ec2e68666efe3
koki-develop <kou.pg.0131@gmail.com>
2022-03-05 15:23:16 +0900

    userAtoms を削除

--- frontend/src/atoms/userAtoms.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from "recoil";
-import { User } from "@/models/user";
-
-export const authenticatedUserState = atom<User | null>({
-  key: "authenticatedUserState",
-  default: null,
-});
