8162522dec1e66e7d01ede0ba1bdbf029eb4d2af
koki-develop <kou.pg.0131@gmail.com>
2022-03-06 15:02:42 +0900

    theme を仮作成

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,20 +1,24 @@
 import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import { theme } from "@/components/Layout/theme";
 import AuthProvider from "@/components/providers/AuthProvider";
 import AppRoutes from "./AppRoutes";
 
 const App: React.VFC = React.memo(() => {
   return (
-    <RecoilRoot>
-      <AuthProvider>
-        <CssBaseline />
-        <BrowserRouter>
-          <AppRoutes />
-        </BrowserRouter>
-      </AuthProvider>
-    </RecoilRoot>
+    <ThemeProvider theme={theme}>
+      <RecoilRoot>
+        <AuthProvider>
+          <CssBaseline />
+          <BrowserRouter>
+            <AppRoutes />
+          </BrowserRouter>
+        </AuthProvider>
+      </RecoilRoot>
+    </ThemeProvider>
   );
 });
 
