cf8414815457fee06542f240edfc2f19ee70238c
koki-develop <kou.pg.0131@gmail.com>
2022-03-06 11:05:39 +0900

    Popper コンポーネントを作成

---
+++ frontend/src/components/utils/Popper.tsx
@@ -0,0 +1,24 @@
+import ClickAwayListener from "@mui/material/ClickAwayListener";
+import MuiPopper, { PopperProps as MuiPopperProps } from "@mui/material/Popper";
+import React from "react";
+
+export type PopperProps = Omit<MuiPopperProps, "children"> & {
+  children: React.ReactElement;
+  onClose: () => void;
+};
+
+const Popper: React.VFC<PopperProps> = React.memo((props) => {
+  const { children, onClose, ...popperProps } = props;
+
+  return (
+    <MuiPopper {...popperProps}>
+      <ClickAwayListener touchEvent={false} onClickAway={onClose}>
+        {children}
+      </ClickAwayListener>
+    </MuiPopper>
+  );
+});
+
+Popper.displayName = "Popper";
+
+export default Popper;
