1d35f935f948a172bcbb68a7da4554ca45f985f2
koki-develop <kou.pg.0131@gmail.com>
2022-03-06 09:08:15 +0900

    認証中はレイアウトを表示しないようにした

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,9 +1,18 @@
+import AppBar from "@mui/material/AppBar";
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { useAuth } from "@/components/providers/AuthProvider";
 
 const Layout: React.VFC = React.memo(() => {
+  const { initialized } = useAuth();
+
+  if (!initialized) {
+    return <Outlet />;
+  }
+
   return (
     <div>
+      <AppBar position="static">hogefuga</AppBar>
       <Outlet />
     </div>
   );
