5dd544858bd4bde3484cc52371a02040d56cc4fa
koki-develop <kou.pg.0131@gmail.com>
2022-03-07 08:56:04 +0900

    SectionListItem を装飾

--- frontend/src/components/model/section/SectionListItem.tsx
+++ frontend/src/components/model/section/SectionListItem.tsx
@@ -1,4 +1,11 @@
-import React, { useCallback } from "react";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import IconButton from "@mui/material/IconButton";
+import React, { useCallback, useState } from "react";
 import { Draggable } from "react-beautiful-dnd";
 import TaskList from "@/components/model/task/TaskList";
 import { Section } from "@/models/section";
@@ -36,6 +43,16 @@ const SectionListItem: React.VFC<SectionListItemProps> = React.memo((props) => {
     onMultiSelectTask,
   } = props;
 
+  const [expanded, setExpanded] = useState<boolean>(true);
+
+  const handleExpand = useCallback(() => {
+    setExpanded(true);
+  }, []);
+
+  const handleCollapse = useCallback(() => {
+    setExpanded(false);
+  }, []);
+
   const handleDelete = useCallback(() => {
     onDelete(section);
   }, [onDelete, section]);
@@ -43,31 +60,50 @@ const SectionListItem: React.VFC<SectionListItemProps> = React.memo((props) => {
   return (
     <Draggable draggableId={section.id} index={section.index}>
       {(provided) => (
-        <div ref={provided.innerRef} {...provided.draggableProps}>
-          <div>
-            <span {...provided.dragHandleProps}>handle</span>
-            <span>
-              {" "}
-              [{section.index}]{section.name}
-            </span>
-            <span>
-              <button onClick={handleDelete}>delete</button>
-            </span>
-          </div>
-          <TaskList
-            projectId={projectId}
-            sectionId={section.id}
-            tasks={tasks}
-            selectedTasks={selectedTasks}
-            onCompleteTask={onCompleteTask}
-            onIncompleteTask={onIncompleteTask}
-            onDeleteTask={onDeleteTask}
-            onCreateTask={onCreateTask}
-            onClickTask={onClickTask}
-            onSelectTask={onSelectTask}
-            onMultiSelectTask={onMultiSelectTask}
-          />
-        </div>
+        <Box
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          sx={{ mb: 2 }}
+        >
+          <Card square {...provided.dragHandleProps}>
+            <CardHeader
+              sx={{ p: 1, pr: 2 }}
+              avatar={
+                expanded ? (
+                  <IconButton size="small" onClick={handleCollapse}>
+                    <ArrowDropDownIcon />
+                  </IconButton>
+                ) : (
+                  <IconButton size="small" onClick={handleExpand}>
+                    <ArrowRightIcon />
+                  </IconButton>
+                )
+              }
+              action={
+                <IconButton onClick={handleDelete}>
+                  <DeleteIcon />
+                </IconButton>
+              }
+              title={`[${section.index}] ${section.name}`}
+              titleTypographyProps={{ variant: "h6" }}
+            />
+          </Card>
+          {expanded && (
+            <TaskList
+              projectId={projectId}
+              sectionId={section.id}
+              tasks={tasks}
+              selectedTasks={selectedTasks}
+              onCompleteTask={onCompleteTask}
+              onIncompleteTask={onIncompleteTask}
+              onDeleteTask={onDeleteTask}
+              onCreateTask={onCreateTask}
+              onClickTask={onClickTask}
+              onSelectTask={onSelectTask}
+              onMultiSelectTask={onMultiSelectTask}
+            />
+          )}
+        </Box>
       )}
     </Draggable>
   );
