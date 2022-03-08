0d8c8f2d1226fea30ad97ca61e74bfa8606ecaf3
koki-develop <kou.pg.0131@gmail.com>
2022-03-08 19:26:49 +0900

    pb を 0 -> 1

--- frontend/src/components/model/project/ProjectForm.tsx
+++ frontend/src/components/model/project/ProjectForm.tsx
@@ -88,7 +88,7 @@ const ProjectForm: React.VFC<ProjectFormProps> = React.memo((props) => {
       <Fade in={open} timeout={200}>
         <Container maxWidth="sm" sx={{ outline: "none" }}>
           <Card>
-            <CardContent sx={{ pb: 0 }}>
+            <CardContent sx={{ pb: 1 }}>
               <Form onSubmit={handleCreate}>
                 <Field>
                   <TextField
