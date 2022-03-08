afaecf9a4883c6659b30983eb496def50ae1376f
koki-develop <kou.pg.0131@gmail.com>
2022-03-08 19:34:44 +0900

    Modal コンポーネントを適用

--- frontend/src/components/model/project/ProjectForm.tsx
+++ frontend/src/components/model/project/ProjectForm.tsx
@@ -3,13 +3,12 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Container from "@mui/material/Container";
-import Fade from "@mui/material/Fade";
-import Modal from "@mui/material/Modal";
 import TextField from "@mui/material/TextField";
 import React, { useCallback, useEffect, useState } from "react";
 import Field from "@/components/utils/Field";
 import Form from "@/components/utils/Form";
 import LoadableButton from "@/components/utils/LoadableButton";
+import Modal from "@/components/utils/Modal";
 import { Project } from "@/models/project";
 import { buildProject } from "@/lib/projectUtils";
 
@@ -73,57 +72,45 @@ const ProjectForm: React.VFC<ProjectFormProps> = React.memo((props) => {
   }, [clearForm, open]);
 
   return (
-    <Modal
-      closeAfterTransition
-      open={open}
-      onClose={onClose}
-      BackdropProps={{ timeout: 200 }}
-      sx={{
-        alignItems: "center",
-        display: "flex",
-        justifyContent: "center",
-      }}
-    >
-      <Fade in={open} timeout={200}>
-        <Container maxWidth="sm" sx={{ outline: "none" }}>
-          <Card sx={{ maxHeight: "calc(100vh - 48px)", overflowY: "auto" }}>
-            <CardContent sx={{ pb: 1 }}>
-              <Form onSubmit={handleCreate}>
-                <Field>
-                  <TextField
-                    autoFocus
-                    fullWidth
-                    disabled={loading}
-                    label="プロジェクト名 *"
-                    value={name}
-                    error={Boolean(nameError)}
-                    helperText={nameError}
-                    onChange={handleChangeName}
-                  />
-                </Field>
-              </Form>
-            </CardContent>
-            <CardActions>
-              <Button
-                fullWidth
-                disabled={loading}
-                onClick={onClose}
-                variant="contained"
-              >
-                キャンセル
-              </Button>
-              <LoadableButton
-                fullWidth
-                loading={loading}
-                onClick={handleCreate}
-                variant="contained"
-              >
-                作成
-              </LoadableButton>
-            </CardActions>
-          </Card>
-        </Container>
-      </Fade>
+    <Modal open={open} onClose={onClose}>
+      <Container maxWidth="sm" sx={{ outline: "none" }}>
+        <Card sx={{ maxHeight: "calc(100vh - 48px)", overflowY: "auto" }}>
+          <CardContent sx={{ pb: 1 }}>
+            <Form onSubmit={handleCreate}>
+              <Field>
+                <TextField
+                  autoFocus
+                  fullWidth
+                  disabled={loading}
+                  label="プロジェクト名 *"
+                  value={name}
+                  error={Boolean(nameError)}
+                  helperText={nameError}
+                  onChange={handleChangeName}
+                />
+              </Field>
+            </Form>
+          </CardContent>
+          <CardActions>
+            <Button
+              fullWidth
+              disabled={loading}
+              onClick={onClose}
+              variant="contained"
+            >
+              キャンセル
+            </Button>
+            <LoadableButton
+              fullWidth
+              loading={loading}
+              onClick={handleCreate}
+              variant="contained"
+            >
+              作成
+            </LoadableButton>
+          </CardActions>
+        </Card>
+      </Container>
     </Modal>
   );
 });
