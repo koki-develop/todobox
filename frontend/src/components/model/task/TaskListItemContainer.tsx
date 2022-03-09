3fd662bf157985fe239baad85556894a88f8d3dd
koki-develop <kou.pg.0131@gmail.com>
2022-03-10 08:05:08 +0900

    TaskListItemContainer を作成、 TaskNewListItem に適用

---
+++ frontend/src/components/model/task/TaskListItemContainer.tsx
@@ -0,0 +1,32 @@
+import ListItem, { ListItemProps } from "@mui/material/ListItem";
+
+import { useTheme } from "@mui/material/styles";
+import React from "react";
+
+export type TaskListItemContainerProps = ListItemProps;
+
+const TaskListItemContainer: React.VFC<TaskListItemContainerProps> =
+  React.forwardRef((props, ref) => {
+    const { children, ...listItemProps } = props;
+    const theme = useTheme();
+
+    return (
+      <ListItem
+        {...listItemProps}
+        ref={ref}
+        sx={{
+          backgroundColor: theme.palette.background.paper,
+          border: "1px solid",
+          borderColor: "divider",
+          height: 38,
+          marginTop: "-1px",
+        }}
+      >
+        {children}
+      </ListItem>
+    );
+  });
+
+TaskListItemContainer.displayName = "TaskListItemContainer";
+
+export default TaskListItemContainer;
--- frontend/src/components/model/task/TaskNewListItem.tsx
+++ frontend/src/components/model/task/TaskNewListItem.tsx
@@ -3,6 +3,7 @@ import ListItemText from "@mui/material/ListItemText";
 import TextField from "@mui/material/TextField";
 import React, { useCallback, useState } from "react";
 import Form from "@/components/utils/Form";
+import TaskListItemContainer from "@/components/model/task/TaskListItemContainer";
 
 export type TaskNewListItemProps = {
   onCreate: (title: string) => void;
@@ -35,31 +36,19 @@ const TaskNewListItem: React.VFC<TaskNewListItemProps> = React.memo((props) => {
   }, [handleSubmit]);
 
   return (
-    <ListItem
-      sx={{
-        backgroundColor: (theme) => theme.palette.background.paper,
-        border: "1px solid",
-        borderColor: "divider",
-        height: 38,
-        marginTop: "-1px",
-      }}
-    >
-      <ListItemText
-        primary={
-          <Form onSubmit={handleSubmit}>
-            <TextField
-              autoFocus
-              placeholder="新しいタスク"
-              variant="standard"
-              onBlur={handleBlur}
-              onChange={handleChangeTitle}
-              value={title}
-              InputProps={{ disableUnderline: true }}
-            />
-          </Form>
-        }
-      />
-    </ListItem>
+    <TaskListItemContainer>
+      <Form onSubmit={handleSubmit}>
+        <TextField
+          autoFocus
+          placeholder="新しいタスク"
+          variant="standard"
+          onBlur={handleBlur}
+          onChange={handleChangeTitle}
+          value={title}
+          InputProps={{ disableUnderline: true }}
+        />
+      </Form>
+    </TaskListItemContainer>
   );
 });
 
