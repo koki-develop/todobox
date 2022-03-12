0246ab5ac6a482465fca46f0ae31f3cd739402ee
koki-develop <kou.pg.0131@gmail.com>
2022-03-12 15:22:35 +0900

    deleteUser 関数を作成

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,4 +1,7 @@
+import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
+
+admin.initializeApp(functions.firebaseConfig() ?? undefined);
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,3 +10,16 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
 });
+
+export const deleteUser = functions
+  .runWith({ timeoutSeconds: 300 })
+  .https.onCall(async (_data, context) => {
+    if (!context.auth) {
+      throw new functions.https.HttpsError(
+        "unauthenticated",
+        "Unauthenticated"
+      );
+    }
+    const { uid } = context.auth;
+    await admin.auth().deleteUser(uid);
+  });
