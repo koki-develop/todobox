6480915b3689c55abaf0e771c905ee6d8d234dee
koki-develop <kou.pg.0131@gmail.com>
2022-03-12 20:01:46 +0900

    projectsInitializedState を追加

--- frontend/src/atoms/projectAtoms.ts
+++ frontend/src/atoms/projectAtoms.ts
@@ -1,6 +1,11 @@
 import { atom } from "recoil";
 import { Project } from "@/models/project";
 
+export const projectsInitializedState = atom<boolean>({
+  key: "projectsInitializedState",
+  default: false,
+});
+
 export const projectsState = atom<Project[]>({
   key: "projectsState",
   default: [],
