d08239d29c01847c60fdf94494d87106ff720dc1
koki-develop <kou.pg.0131@gmail.com>
2022-03-12 15:45:19 +0900

    親リソースを削除したときに子リソースを削除するようにした

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -3,6 +3,9 @@ import * as functions from "firebase-functions";
 
 admin.initializeApp(functions.firebaseConfig() ?? undefined);
 
+const firestore = admin.firestore();
+const functionBuilder = functions.runWith({ timeoutSeconds: 300 });
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
@@ -11,9 +14,8 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
 
-export const deleteUser = functions
-  .runWith({ timeoutSeconds: 300 })
-  .https.onCall(async (_data, context) => {
+export const deleteUser = functionBuilder.https.onCall(
+  async (_data, context) => {
     if (!context.auth) {
       throw new functions.https.HttpsError(
         "unauthenticated",
@@ -22,4 +24,37 @@ export const deleteUser = functions
     }
     const { uid } = context.auth;
     await admin.auth().deleteUser(uid);
+  }
+);
+
+export const handleDeleteUser = functionBuilder.auth
+  .user()
+  .onDelete(async (user) => {
+    const snapshot = await firestore
+      .collection(`users/${user.uid}/projects`)
+      .get();
+    const docs = snapshot.docs;
+    for (const doc of docs) {
+      await doc.ref.delete();
+    }
+  });
+
+export const handleDeleteProject = functionBuilder.firestore
+  .document("users/{userId}/projects/{projectId}")
+  .onDelete(async (_snapshot, context) => {
+    const { userId, projectId } = context.params;
+
+    const sectionsSnapshot = await firestore
+      .collection(`users/${userId}/projects/${projectId}/sections`)
+      .get();
+    const tasksSnapshot = await firestore
+      .collection(`users/${userId}/projects/${projectId}/tasks`)
+      .get();
+
+    for (const doc of sectionsSnapshot.docs) {
+      await doc.ref.delete();
+    }
+    for (const doc of tasksSnapshot.docs) {
+      await doc.ref.delete();
+    }
   });
