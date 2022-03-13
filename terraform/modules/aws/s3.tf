b03bd2d51e7b772a5a22376f9ec70381e4be37f4
koki-develop <kou.pg.0131@gmail.com>
2022-03-13 21:39:39 +0900

    パブリックアクセスをブロックする設定を追加

--- terraform/modules/aws/s3.tf
+++ terraform/modules/aws/s3.tf
@@ -6,3 +6,11 @@ resource "aws_s3_bucket_acl" "frontend" {
   bucket = aws_s3_bucket.frontend.id
   acl    = "private"
 }
+
+resource "aws_s3_bucket_public_access_block" "frontend" {
+  bucket                  = aws_s3_bucket.frontend.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
