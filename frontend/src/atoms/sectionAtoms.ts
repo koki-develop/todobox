3e59d0b550f881aa0af159b5627d7c8310d5fd29
koki-develop <kou.pg.0131@gmail.com>
2022-03-13 11:00:22 +0900

    sectionsInitializedState を追加

--- frontend/src/atoms/sectionAtoms.ts
+++ frontend/src/atoms/sectionAtoms.ts
@@ -1,6 +1,11 @@
 import { atom } from "recoil";
 import { Section } from "@/models/section";
 
+export const sectionsInitializedState = atom<boolean>({
+  key: "sectionsInitializedState",
+  default: false,
+});
+
 export const sectionsState = atom<Section[]>({
   key: "sectionsState",
   default: [],
