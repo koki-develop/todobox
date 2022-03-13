3c99e43ba8ff402c10859f6aef1a732c7ada750e
koki-develop <kou.pg.0131@gmail.com>
2022-03-13 14:51:01 +0900

    tasks 関連の state を作成

---
+++ frontend/src/atoms/taskAtom.ts
@@ -0,0 +1,22 @@
+import { atom } from "recoil";
+import { Task } from "@/models/task";
+
+export const incompletedTasksInitializedState = atom<boolean>({
+  key: "incompletedTasksInitializedState",
+  default: false,
+});
+
+export const incompletedTasksState = atom<Task[]>({
+  key: "incompletedTasksState",
+  default: [],
+});
+
+export const completedTasksInitializedState = atom<boolean>({
+  key: "completedTasksInitializedState",
+  default: false,
+});
+
+export const completedTasksState = atom<Task[]>({
+  key: "completedTasksState",
+  default: [],
+});
