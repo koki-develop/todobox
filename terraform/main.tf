5e6f7ccf8204e7a6782b4b74b89d982620648ca1
koki-develop <kou.pg.0131@gmail.com>
2022-03-13 21:31:04 +0900

    backend の設定を作成

---
+++ terraform/main.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    profile              = "default"
+    workspace_key_prefix = "workspaces"
+    region               = "us-east-1"
+    bucket               = "todo-box-tfstates"
+    key                  = "terraform.tfstate"
+  }
+}
