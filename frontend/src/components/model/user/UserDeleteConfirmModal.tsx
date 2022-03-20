1d48492e34dc218718d3c64385b2b1c055f31f8d
koki-develop <kou.pg.0131@gmail.com>
2022-03-20 21:07:09 +0900

    アカウント削除時に確認メッセージを表示するようにした

---
+++ frontend/src/components/model/user/UserDeleteConfirmModal.tsx
@@ -0,0 +1,59 @@
+import Button from "@mui/material/Button";
+import CardContent from "@mui/material/CardContent";
+import React, { useCallback, useState } from "react";
+import LoadableButton from "@/components/utils/LoadableButton";
+import ModalCard from "@/components/utils/ModalCard";
+import ModalCardActions from "@/components/utils/ModalCardActions";
+import ModalCardHeader from "@/components/utils/ModalCardHeader";
+import { useCurrentUser } from "@/hooks/userHooks";
+
+export type UserDeleteConfirmModalProps = {
+  open: boolean;
+
+  onCancel: () => void;
+  onDeleted: () => void;
+};
+
+const UserDeleteConfirmModal: React.VFC<UserDeleteConfirmModalProps> =
+  React.memo((props) => {
+    const { open, onCancel, onDeleted } = props;
+
+    const { deleteAccount } = useCurrentUser();
+
+    const [deleting, setDeleting] = useState<boolean>(false);
+
+    const handleDelete = useCallback(() => {
+      setDeleting(true);
+      deleteAccount()
+        .then(() => {
+          onDeleted();
+        })
+        .finally(() => {
+          setDeleting(false);
+        });
+    }, [deleteAccount, onDeleted]);
+
+    return (
+      <ModalCard open={open} onClose={onCancel}>
+        <ModalCardHeader title="アカウントを削除しますか？" />
+        <CardContent>
+          アカウントとそれに紐づく全てのデータを完全に削除します。この操作は取り消せません。
+        </CardContent>
+        <ModalCardActions>
+          <Button onClick={onCancel}>キャンセル</Button>
+          <LoadableButton
+            variant="contained"
+            color="error"
+            loading={deleting}
+            onClick={handleDelete}
+          >
+            削除
+          </LoadableButton>
+        </ModalCardActions>
+      </ModalCard>
+    );
+  });
+
+UserDeleteConfirmModal.displayName = "UserDeleteConfirmModal";
+
+export default UserDeleteConfirmModal;
--- frontend/src/components/pages/SettingsPage/SettingsPage.tsx
+++ frontend/src/components/pages/SettingsPage/SettingsPage.tsx
@@ -1,30 +1,63 @@
+import Alert from "@mui/material/Alert";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import { useTheme } from "@mui/material/styles";
 import { User } from "firebase/auth";
 import React, { useCallback, useState } from "react";
-import { useCurrentUser } from "@/hooks/userHooks";
+import UserDeleteConfirmModal from "@/components/model/user/UserDeleteConfirmModal";
+import Field from "@/components/utils/Field";
 
 export type SettingsPageProps = {
   currentUser: User;
 };
 
-const SettingsPage: React.VFC<SettingsPageProps> = React.memo((props) => {
-  const { currentUser } = props;
-  const { deleteAccount } = useCurrentUser();
+const SettingsPage: React.VFC<SettingsPageProps> = React.memo(() => {
+  const theme = useTheme();
 
-  const [deleting, setDeleting] = useState<boolean>(false);
+  const [openDeleteConfirm, setOpenDeleteConfirm] = useState<boolean>(false);
 
   const handleClickDeleteAccount = useCallback(async () => {
-    setDeleting(true);
-    await deleteAccount().finally(() => {
-      setDeleting(false);
-    });
-  }, [deleteAccount]);
+    setOpenDeleteConfirm(true);
+  }, []);
+
+  const handleCloseDeleteConfirm = useCallback(() => {
+    setOpenDeleteConfirm(false);
+  }, []);
 
   return (
-    <div>
-      {deleting && "deleting..."}
-      {currentUser.uid}
-      <button onClick={handleClickDeleteAccount}>delete account</button>
-    </div>
+    <Container maxWidth="lg" sx={{ pt: 2 }}>
+      <UserDeleteConfirmModal
+        open={openDeleteConfirm}
+        onCancel={handleCloseDeleteConfirm}
+        onDeleted={handleCloseDeleteConfirm}
+      />
+      <Field>
+        <Alert
+          icon={false}
+          severity="error"
+          sx={{ border: "1px solid", borderColor: theme.palette.error.main }}
+        >
+          <Field>
+            <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+              アカウントを削除
+            </Typography>
+            <Typography>
+              アカウントとそれに紐づく全てのデータを完全に削除します。この操作は取り消せません。
+            </Typography>
+          </Field>
+          <Field>
+            <Button
+              color="error"
+              variant="contained"
+              onClick={handleClickDeleteAccount}
+            >
+              アカウントを削除
+            </Button>
+          </Field>
+        </Alert>
+      </Field>
+    </Container>
   );
 });
 
