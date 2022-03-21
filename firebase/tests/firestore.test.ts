6a58712ad26f4cc39eaadd2751c1bd7495e62b44
koki-develop <kou.pg.0131@gmail.com>
2022-03-21 15:07:14 +0900

    空のテストケースを作成

--- firebase/tests/firestore.test.ts
+++ firebase/tests/firestore.test.ts
@@ -1,5 +1,54 @@
-import { it, expect } from "vitest";
+import { initializeTestEnvironment } from "@firebase/rules-unit-testing";
+import fs from "fs";
+import { it, describe, expect, beforeEach } from "vitest";
 
-it("hello vitest", () => {
-  expect(1).toEqual(1);
+const PROJECT_ID = "test-todo-box";
+
+const getTestEnvironment = async () => {
+  const firestoreRules = fs.readFileSync("firestore.rules", "utf8");
+
+  return await initializeTestEnvironment({
+    projectId: PROJECT_ID,
+    firestore: {
+      rules: firestoreRules,
+    },
+  });
+};
+
+const getAuthenticatedContext = async (uid: string) => {
+  const testEnv = await getTestEnvironment();
+  return testEnv.authenticatedContext(uid);
+};
+
+const getUnauthenticatedContext = async () => {
+  const testEnv = await getTestEnvironment();
+  return testEnv.unauthenticatedContext();
+};
+
+const getAuthenticatedFirestore = async (uid: string) => {
+  const context = await getAuthenticatedContext(uid);
+  return context.firestore();
+};
+
+const getUnauthenticatedFirestore = async () => {
+  const context = await getUnauthenticatedContext();
+  return context.firestore();
+};
+
+describe("/users/{userId}", () => {
+  describe("/projects/{projectId}", () => {
+    it.todo("pending");
+  });
+
+  describe("/counters/tasks/shards/{shardId}", () => {
+    it.todo("pending");
+  });
+
+  describe("/sections/{sectionId}", () => {
+    it.todo("pending");
+  });
+
+  describe("/tasks/{taskId}", () => {
+    it.todo("pending");
+  });
 });
