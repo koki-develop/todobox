a80d94729760db200dda5e8e21c62b18973d6c61
koki-develop <kou.pg.0131@gmail.com>
2022-07-23 20:09:20 +0900

    main.tf から backend.tf, variables.tf を分割

--- terraform/app/main.tf
+++ terraform/app/main.tf
@@ -1,23 +1,3 @@
-variable "basicauth_username" {
-  type      = string
-  sensitive = true
-}
-
-variable "basicauth_password" {
-  type      = string
-  sensitive = true
-}
-
-terraform {
-  backend "s3" {
-    workspace_key_prefix = "workspaces"
-    region               = "us-east-1"
-    bucket               = "todobox-tfstates"
-    key                  = "terraform.tfstate"
-    encrypt              = true
-  }
-}
-
 module "aws" {
   source = "./modules/aws"
   stage  = terraform.workspace
---
+++ terraform/app/variables.tf
@@ -0,0 +1,9 @@
+variable "basicauth_username" {
+  type      = string
+  sensitive = true
+}
+
+variable "basicauth_password" {
+  type      = string
+  sensitive = true
+}
